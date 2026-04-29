a46f31793b8fcdc23eb79d02a67e9c0f2ec3d119
Eric <ecuzziol@gmail.com>
2026-04-29 17:58:06 -0300

    formato de moneda en precio

--- resources/views/components/product-item.blade.php
+++ resources/views/components/product-item.blade.php
@@ -8,7 +8,7 @@
         <div class="card-body d-flex flex-column text-center">
             <h5 class="card-title">{{ $productTitle }}</h5>
             <h5 class="card-title">{{ $productConsole }}</h5>
-            <p class="price">${{ $price }}</p>
+            <p class="price">$ {{ number_format($price, 0, ',', '.') }}</p>
 
             <div class="mt-auto">
                 <button class="btn btn-retro w-100">Comprar</button>
